c0ed7464cbc1d83a23ef626ebf9358b6f1f7bab3
Amit Prakash <amitprakash067@gmail.com>
2023-09-06 18:26:30 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import NavBar from "./components/Nav";
+import {
+  CustomerReviews,
+  Footer,
+  Hero,
+  PopularProduct,
+  Service,
+  SpecialOffer,
+  Subscribe,
+  SuperQuality,
+} from "./sections";
+
+const App = () => {
+  return (
+    <main className='relative'>
+      <NavBar />
+      <section className='xl:padding-l wide:padding-r padding-b'>
+        <Hero />
+      </section>
+      <section className='padding'>
+        <PopularProduct />
+      </section>
+      <section className='padding'>
+        <SuperQuality />
+      </section>
+      <section className='padding-x py-10'>
+        <Service />
+      </section>
+      <section className='padding'>
+        <SpecialOffer />
+      </section>
+      <section className='bg-pale-blue padding'>
+        <CustomerReviews />
+      </section>
+      <section className='padding-x sm:py-32 py-16 w-full'>
+        <Subscribe />
+      </section>
+      <section className=' bg-black padding-x padding-t pb-8'>
+        <Footer />
+      </section>
+    </main>
+  );
+};
+
+export default App;
